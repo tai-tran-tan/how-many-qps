b25dac70fd047b35684b1195745138162c801c5f
Tai Tran <tttai.tai.tran@gmail.com>
2024-09-22 18:22:07 +0700

    build and deploy instruction

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ import Aura from '@primevue/themes/aura';
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+  ssr: false,
   modules: [
     '@primevue/nuxt-module'
   ],
